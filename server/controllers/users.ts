d24760749570761af4ecf216e2357360da3e173f
robertoooc <robertooo.c48@gmail.com>
2023-02-11 15:21:56 -0800

    have working hashing out passwords and webtokens

--- server/controllers/users.ts
+++ server/controllers/users.ts
@@ -1,29 +1,81 @@
 import express, {Express, Request, Response} from 'express'
 import User from '../models/User.js'
 import { dbConnect } from '../models/index.js'
+import { genSaltSync, hashSync, compareSync } from 'bcrypt-ts'
+import jwt from 'jsonwebtoken'
 dbConnect()
 const router = express.Router()
+declare var process : {
+    env : {
+        JWT_SECRET: string
+    }
+}   
 
 router.get('/', async(req: Request,res: Response)=>{
     try{
         const findUser = await User.findById(req.body.id)
         if (findUser){
-            res.status(200).json(findUser)
+            return res.status(200).json(findUser)
         }
         else{
-            res.status(404).json({message:" User not found "})
+            return res.status(404).json({message:" User not found "})
         }
     }catch(err){
         res.status(500).json({message: 'My bad'})
     }
 })
 
-router.post('/', async(req:Request,res:Response)=>{
-    try{
+// router.post('/', async(req:Request,res:Response)=>{
+//     try{
+//         const newUser = new User(req.body)
+//         const saveUser = await newUser.save()
+//         res.json({saveUser})
+//     }catch(err){
+//         res.status(500).json({message: 'My bad'})
+//     }
+// })
 
+router.delete('/', async (req:Request, res: Response)=>{
+    try{
+        const deleteUser = await User.findOneAndDelete({_id: req.body.id})
+        if(!deleteUser){
+            return res.status(404).json({message:'User not found'})
+        }
+        return res.status(200).json({message: 'user deleted', deleteUser})
     }catch(err){
-        res.status(500).json({message: 'My bad'})
+        res.status(500).json({message:'My bad'})
     }
 })
 
+router.post('/register', async(req:Request,res:Response)=>{
+    try{
+        // checking if new user already exists in db
+        const findUser = await User.findOne({
+            email: req.body.email
+        })
+        if (findUser) return res.status(400).json({message: 'User already exists'})
+    
+        const password:string = req.body.password
+        const saltRounds:number = 12
+        const salt = genSaltSync(saltRounds)
+        const hashedPassword = hashSync(password, salt)
+        console.log(hashedPassword)
+
+        const newUser = await User.create({
+            name: req.body.name, 
+            email: req.body.email,
+            password: hashedPassword
+        })
+        const jwtPayload: {name: string, email: string, id: string} = {
+            name: newUser.name,
+            email: newUser.email,
+            id: newUser.id,
+        }
+        const secret = process.env.JWT_SECRET 
+        const token = await jwt.sign(jwtPayload,secret)
+        res.json({token})
+    }catch(err){
+        res.status(500).json({message: 'My Bad'})
+    }
+})
 export default router
--- server/dist/controllers/users.js
+++ server/dist/controllers/users.js
@@ -1,20 +1,74 @@
 import express from 'express';
 import User from '../models/User.js';
 import { dbConnect } from '../models/index.js';
+import { genSaltSync, hashSync } from 'bcrypt-ts';
+import jwt from 'jsonwebtoken';
 dbConnect();
 const router = express.Router();
 router.get('/', async (req, res) => {
     try {
         const findUser = await User.findById(req.body.id);
         if (findUser) {
-            res.status(200).json(findUser);
+            return res.status(200).json(findUser);
         }
         else {
-            res.status(404).json({ message: " User not found " });
+            return res.status(404).json({ message: " User not found " });
         }
     }
     catch (err) {
         res.status(500).json({ message: 'My bad' });
+    }
+});
+router.post('/', async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        const saveUser = await newUser.save();
+        res.json({ saveUser });
+    }
+    catch (err) {
+        res.status(500).json({ message: 'My bad' });
+    }
+});
+router.delete('/', async (req, res) => {
+    try {
+        const deleteUser = await User.findOneAndDelete({ _id: req.body.id });
+        if (!deleteUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        return res.status(200).json({ message: 'user deleted', deleteUser });
+    }
+    catch (err) {
+        res.status(500).json({ message: 'My bad' });
+    }
+});
+router.post('/register', async (req, res) => {
+    try {
+        // checking if new user already exists in db
+        const findUser = await User.findOne({
+            email: req.body.email
+        });
+        if (findUser)
+            return res.status(400).json({ message: 'User already exists' });
+        const password = req.body.password;
+        const saltRounds = 12;
+        const salt = genSaltSync(saltRounds);
+        const hashedPassword = hashSync(password, salt);
+        const newUser = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        const jwtPayload = {
+            name: newUser.name,
+            email: newUser.email,
+            id: newUser.id,
+        };
+        const secret = process.env.JWT_SECRET;
+        const token = await jwt.sign(jwtPayload, secret);
+        res.json({ token });
+    }
+    catch (err) {
+        res.status(500).json({ message: 'My Bad' });
     }
 });
 export default router;
