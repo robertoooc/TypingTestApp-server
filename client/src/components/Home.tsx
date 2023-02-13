0b6617ce4f1cf21b87b04a9578f0e18f4ac13ece
robertoooc <robertooo.c48@gmail.com>
2023-02-13 10:52:55 -0800

    logic for keeping track user is typing correct words is coming together, edge case of two letters next to each other being same needs resolving

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -10,23 +10,45 @@ interface Props {
     currentUser: currentUser|null;
  }
 const Home:FC<Props>=({currentUser})=>{
-    const [words,setWords]=useState([])
-    const [wordsTyped,setWordsTyped]=useState([])
-    const [commonMistake,setCommonMistake]=useState<string|null>('')
+    const [words,setWords]=useState<string>('')
+    const [index,setIndex]=useState<number>(0)
+    const [currentChar, setCurrentChar]=useState<string|null>('')
+    const [mistakes,setMistakes]=useState<Array<string>|null>([])
+    const [userKey, setUserKey]= useState<string|null>('')
+
     useEffect(()=>{
         const pingWords=async()=>{
             try{
                 const response = await axios.get(`https://api.datamuse.com/words?sp=a*`)
                 const listWords = response.data.map((word:{word: string, score:number})=>{
-                    return `${word.word} `
+                    return `${word.word}`
                 })
-                setWords(listWords)
+                const joined = listWords.join(' ')
+                setWords(joined)
+                setCurrentChar(words[index])
             }catch(err){
                 console.log(err)
             }
         }
         pingWords()
     },[])
+
+    useEffect(()=>{
+        document.addEventListener('keydown',(e)=>{
+            setUserKey(e.key)
+        })
+    },[])
+
+    useEffect(()=>{
+        console.log(userKey)
+        if(words[index]=== userKey){
+            console.log('true')
+            let newIndex = index +1
+            setIndex(newIndex)
+        }else{
+            console.log('noooo')
+        }
+    },[userKey])
     return(
         <>
 
