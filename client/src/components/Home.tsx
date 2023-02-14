78e28900ed692bd7dd4bd8c6c9db6081ec8e7fe3
robertoooc <robertooo.c48@gmail.com>
2023-02-13 16:22:05 -0800

    starting to implement timer, very buggy need to come back to

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,84 +1,3 @@
-// import { FC ,useState, useEffect } from "react";
-// import axios from 'axios'
-// interface currentUser{
-//     name?: string;
-//     email?: string;
-//     id?: string;
-//     iat?: number
-// }
-// interface Props {
-//     currentUser: currentUser|null;
-//  }
-// const Home:FC<Props>=({currentUser})=>{
-//     const [words,setWords]=useState<string>('')
-//     const [index,setIndex]=useState<number>(0)
-//     const [currentChar, setCurrentChar]=useState<string|null>('')
-//     const [mistakes,setMistakes]=useState<Array<string>|null>([])
-//     const [userKey, setUserKey]= useState<string|null>('')
-//     const [start, setStart] = useState<Boolean>(false)
-    
-//     const check=(char:string)=>{
-//         if(start){
-//             console.log(char, words[index])
-//             if(words[index]===char){
-//                 console.log('true')
-//                 let newIndex = index +1
-//                 setIndex(newIndex)
-//             }else{
-//                 console.log('noooo')
-//             }
-//         }
-//     }
-
-//     useEffect(()=>{
-//         console.log(start)
-//         const pingWords=async()=>{
-//             try{
-//                 const response = await axios.get(`https://api.datamuse.com/words?sp=a*`)
-//                 const listWords = response.data.map((word:{word: string, score:number})=>{
-//                     return `${word.word}`
-//                 })
-//                 const joined = listWords.join(' ')
-//                 setWords(joined)
-//                 setCurrentChar(words[index])
-//             }catch(err){
-//                 console.log(err)
-//             }
-//         }
-//         pingWords()
-//     },[start])
-
-//     useEffect(()=>{
-//         if(start){
-//             document.addEventListener('keydown',(e)=>{
-//                 console.log('in')
-//                 setUserKey(e.key)
-//                 check(e.key)
-//             })
-//         }
-//     },[])
-
-//     // useEffect(()=>{
-//     //     console.log(userKey)
-//     //     if(words[index]=== userKey){
-//     //         console.log('true')
-//     //         let newIndex = index +1
-//     //         setIndex(newIndex)
-//     //     }else{
-//     //         console.log('noooo')
-//     //     }
-//     // },[userKey])
-//     return(
-//         <>
-
-//         Home
-//         <br></br>
-//         {words}
-//         <button onClick={()=>setStart(true)}>Start</button>
-//         </>
-//     )
-// }
-// export default Home
 import { FC ,useState, useEffect } from "react";
 import axios from 'axios'
 interface currentUser{
@@ -93,11 +12,11 @@ interface Props {
 const Home:FC<Props>=({currentUser})=>{
     const [words,setWords]=useState<string>('')
     const [index,setIndex]=useState<number>(0)
-    const [currentChar, setCurrentChar]=useState<string|null>('')
     const [mistakes,setMistakes]=useState<string[]>([])
     const [userKey, setUserKey]= useState<string|null>('')
-    const [double, setDouble] = useState<Boolean>(false)
     const [load,setLoad] = useState<Boolean>(false)
+    const [time, setTime] = useState<number>(0)
+    const [started,setStarted] = useState<Boolean>(false)
     useEffect(()=>{
         const pingWords=async()=>{
             try{
@@ -107,7 +26,6 @@ const Home:FC<Props>=({currentUser})=>{
                 })
                 const joined = listWords.join(' ')
                 setWords(joined)
-                setCurrentChar(words[index])
             }catch(err){
                 console.log(err)
             }
@@ -117,7 +35,7 @@ const Home:FC<Props>=({currentUser})=>{
     },[])
 
     useEffect(()=>{
-        if(load){
+        if(load&& started){
                 document.addEventListener('keydown',(e)=>{
                     setUserKey(e.key)
                 })
@@ -125,7 +43,8 @@ const Home:FC<Props>=({currentUser})=>{
                     setUserKey(null)
                 } )
         }
-        },[load])
+        },[load,time])
+
 
     useEffect(()=>{
         if(userKey!=null){
@@ -145,30 +64,19 @@ const Home:FC<Props>=({currentUser})=>{
             }
         }
     },[userKey])
-    // useEffect(()=>{
-    //     if(double){
-    //         console.log('in hte loop')
-    //         document.addEventListener('keydown',(e)=>{
-    //         if(e.key == words[index]){
-    //             console.log(e.key)
-    //             let newIndex = index +1
-    //             setIndex(newIndex)
-    //             setDouble(false) 
-    //             return 
-    //         }else{
-    //             console.log(`wrong, : ${e.key} shouldve been ${words[index]}`)
-    //             setDouble(false) 
-    //             setUserKey(e.key)
-    //         console.log('exit loop')
-    //         return 
-    //         }
-    //     })
-    //     document.addEventListener('keyup',()=>{
-    //         console.log('leave')
-    //         return 
-    //     })
-    // }
-    // },[double==true])
+
+useEffect(()=>{
+    if(started){
+        setInterval(()=>{
+            if(time == 60){
+                setTime(-1)
+            }else{
+                let newTime: number = time +1
+                setTime(newTime)
+            }
+        },1000)
+    }
+},[started,time])
 
     const typedText = words.substring(0,index)
     const untypedText = words.substring(index, words.length-1)
@@ -178,13 +86,17 @@ const Home:FC<Props>=({currentUser})=>{
         Home
 
         <div>
+            {time}
             <br></br>
+            <div style={{backgroundColor: "grey"}} onClick={()=>setStarted(true)}>
+                {started == false? <p>click on me to start</p>:null}
             <span style={{color: "grey"}}>
             {typedText}
             </span>
             <span>
             {untypedText}
             </span>
+            </div>
         </div>
 
         </>
